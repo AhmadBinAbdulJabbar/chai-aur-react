f21b287d51a860fd50436d607ed8e349fade567b
ahmadbinabduljabbar <ahmadbinabduljabbar@gmail.com>
2024-01-28 21:33:30 +0500

    till class 3

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      {/* <h1>Chai aur react | Ahmad Bin Abdul Jabbar</h1> */}
+      <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import  Chai from "./chai";
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur react with vite | Ahmad Bin Abdul Jabbar</h1>
+      <Chai />
+      <p> test the return </p>
+    </>
+  )
+}
+
+export default App
